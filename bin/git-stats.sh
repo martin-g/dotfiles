ee94784b2fb037953e99494f20770f5df5f66f07
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2012-11-29 09:53:29 +0100

    git-new-workdir - copy of the official one. Creates a new working copy/local repo that uses symlinks to the original repo on local machine git-stats.sh - a simple script that shows which git commands I use the most

---
+++ bin/git-stats.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+history|grep git| awk '{CMD[$3]++;count++;}END { for (a in CMD)print CMD[a] " " CMD[a]/count*100 "% " a;}' | grep -v "./" | column -c3 -s " " -t | sort -nr | nl |  head -n10
