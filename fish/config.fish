ca578eab1427a2e741c014b92703e00c9d33bc91
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2016-10-18 22:17:44 +0200

    Wahoo is dead. Use powerline-shell for the fancy prompt

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,4 @@
-set -g WAHOO_PATH $HOME/git/dotfiles/.wahoo
-set -g WAHOO_CUSTOM $HOME/git/dotfiles/.wahoo-mine
-source $WAHOO_PATH/init.fish
+function fish_prompt
+    ~/bin/powerline-shell.py $status --shell bare ^/dev/null
+end
 
-set fish_function_path $fish_function_path "/home/martin/.local/lib/python2.7/site-packages/powerline/bindings/fish"
-powerline-setup
